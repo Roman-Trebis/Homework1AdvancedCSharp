3c22e2c968a5c54ca2aaa5c6676ee8e000a7c2a8
31 <roma.trebis@bk.ru>
2023-12-19 18:52:51 +0300

    Update
    Update

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -1,53 +1,46 @@
 ﻿using System;
 
-/* 
- * Задание 1.
- * 1. Написать программу-калькулятор, вычисляющую выражения вида a + b, a - b, a / b, a * b, введенные из командной строки, и выводящую результат выполнения на экран.
- *
-*/
-
-public class Program
+namespace ConsoleApp14
 {
-    static void Main(string[] args)
+    /* 
+    * Задание 1.
+    * 1. Написать программу-калькулятор, вычисляющую выражения вида a + b, a - b, a / b, a * b, введенные из командной строки, и выводящую результат выполнения на экран.
+    *
+    */
+
+    public class Program
     {
-        Console.WriteLine("Введите выражение вида a + b, a - b, a / b, a * b:");
-        string input = Console.ReadLine();
-
-        char[] delimiters = { '+', '-', '/', '*' };
-        string[] tokens = input.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
-        char operation = input.FirstOrDefault(c => c == '+' || c == '-' || c == '/' || c == '*');
-
-        if (tokens.Length != 2 || operation == '0')
+        static void Main(string[] args)
         {
-            Console.WriteLine("Неверный формат выражения");
-            return;
+            if (args.Length == 3)
+            {
+                int result = 0;
+                int number1 = int.Parse(args[0]);
+                string operation = args[1];
+                int number2 = int.Parse(args[2]);
+                if (operation == "+")
+                {
+                    result = number1 + number2;
+                }
+                if (operation == "-")
+                {
+                    result = number1 - number2;
+                }
+                if (operation == "/")
+                {
+                    result = number1 / number2;
+                }
+                if (operation == "*")
+                {
+                    result = number1 * number2;
+                }
+                Console.WriteLine("Результат выполнения операции (" + operation + ") = " + result);
+            }
+            else
+            {
+                Console.WriteLine("Для использования программы введите: число знак(+ - / *) число");
+            }
+            Console.ReadKey();
         }
-
-        if (!double.TryParse(tokens[0], out double a) || !double.TryParse(tokens[1], out double b))
-        {
-            Console.WriteLine("Неверный формат чисел");
-            return;
-        }
-
-        double result = 0;
-        switch (operation)
-        {
-            case '+':
-                result = a + b;
-                break;
-            case '-':
-                result = a - b;
-                break;
-            case '/':
-                result = a / b;
-                break;
-            case '*':
-                result = a * b;
-                break;
-            default:
-                break;
-        }
-
-        Console.WriteLine("Результат: " + result);
     }
 }
